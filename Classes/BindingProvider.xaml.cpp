31b086f9356170da6b9ae1508eef2bb00ae9a2a5
Stevo Brock <stevo@sunsetmagicwerks.com>
2024-09-04 22:23:30 -0700

    Added BindingProvider.

---
+++ Classes/BindingProvider.xaml.cpp
@@ -0,0 +1,93 @@
+﻿//----------------------------------------------------------------------------------------------------------------------
+//	BindingProvider.xaml.cpp			©2024 Stevo Brock		All rights reserved.
+//----------------------------------------------------------------------------------------------------------------------
+
+#include "BindingProvider.xaml.h"
+
+#if __has_include("WinRTToolbox.BindingProvider.g.cpp")
+	#include "WinRTToolbox.BindingProvider.g.cpp"
+#endif
+
+using namespace winrt::WinRTToolbox::implementation;
+
+using hstring = winrt::hstring;
+
+//----------------------------------------------------------------------------------------------------------------------
+// MARK: BindingProvider::Internals
+
+class BindingProvider::Internals {
+	public:
+		Internals(const I<BindingProviderAdapter>& bindingProviderAdapter) :
+			mBindingProviderAdapter(bindingProviderAdapter)
+			{}
+
+		I<BindingProviderAdapter>			mBindingProviderAdapter;
+
+		event<PropertyChangedEventHandler>	mPropertyChangedEvent;
+};
+
+//----------------------------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------------------
+// MARK: BindingProvider
+
+// MARK: Lifecycle methods
+
+//----------------------------------------------------------------------------------------------------------------------
+BindingProvider::BindingProvider(const IInspectable& bindingProviderAdapter)
+//----------------------------------------------------------------------------------------------------------------------
+{
+	mInternals = new Internals(bindingProviderAdapter.as<BindingProviderAdapterCVInspectable>()->GetValue());
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+BindingProvider::~BindingProvider()
+//----------------------------------------------------------------------------------------------------------------------
+{
+	delete mInternals;
+}
+
+// MARK: Instance methods
+
+//----------------------------------------------------------------------------------------------------------------------
+bool BindingProvider::Boolean1() const
+//----------------------------------------------------------------------------------------------------------------------
+{
+	return mInternals->mBindingProviderAdapter->getBool1();
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+void BindingProvider::Boolean1(bool value)
+//----------------------------------------------------------------------------------------------------------------------
+{
+	mInternals->mBindingProviderAdapter->setBool1(value);
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+hstring BindingProvider::String1() const
+//----------------------------------------------------------------------------------------------------------------------
+{
+	return mInternals->mBindingProviderAdapter->getString1();
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+void BindingProvider::String1(hstring value)
+//----------------------------------------------------------------------------------------------------------------------
+{
+	mInternals->mBindingProviderAdapter->setString1(value);
+}
+
+// MARK: Event methods
+
+//----------------------------------------------------------------------------------------------------------------------
+event_token BindingProvider::PropertyChanged(const PropertyChangedEventHandler& handler)
+//----------------------------------------------------------------------------------------------------------------------
+{
+	return mInternals->mPropertyChangedEvent.add(handler);
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+void BindingProvider::PropertyChanged(const event_token& token) noexcept
+//----------------------------------------------------------------------------------------------------------------------
+{
+	mInternals->mPropertyChangedEvent.remove(token);
+}
---
+++ Classes/BindingProvider.xaml.h
@@ -0,0 +1,87 @@
+﻿//----------------------------------------------------------------------------------------------------------------------
+//	BindingProvider.xaml.h			©2024 Stevo Brock		All rights reserved.
+//----------------------------------------------------------------------------------------------------------------------
+
+#pragma once
+
+#include "WinRTToolbox.BindingProvider.g.h"
+#include "TInspectable.h"
+#include "TWrappers.h"
+
+using PropertyChangedEventHandler = winrt::Microsoft::UI::Xaml::Data::PropertyChangedEventHandler;
+
+//----------------------------------------------------------------------------------------------------------------------
+// MARK: winrt::WinRTToolbox
+
+namespace winrt::WinRTToolbox {
+
+	// MARK: BindingProviderAdapter
+	class BindingProviderAdapter {
+		// Methods
+		public:
+							// Lifecycle methods
+							BindingProviderAdapter() {}
+			virtual			~BindingProviderAdapter() {}
+
+							// Instance methods
+			virtual	bool	getBool1() const { return false; }
+			virtual	void	setBool1(bool value) {}
+
+			virtual	hstring	getString1() const { return L""; }
+			virtual	void	setString1(hstring value) {}
+	};
+
+	 // MARK: - BindingProviderAdapterCVInspectable
+
+	struct BindingProviderAdapterCVInspectable :
+			public TCVInspectable<BindingProviderAdapterCVInspectable, I<BindingProviderAdapter> > {
+		// Methods
+		public:
+			BindingProviderAdapterCVInspectable(I<BindingProviderAdapter>& bindingProviderAdapter) :
+				TCVInspectable<BindingProviderAdapterCVInspectable, I<BindingProviderAdapter> >(bindingProviderAdapter)
+				{}
+	};
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------------------
+// MARK: winrt::WinRTToolbox::implementation
+
+namespace winrt::WinRTToolbox::implementation {
+
+	// MARK: BindingProvider
+	struct BindingProvider : BindingProviderT<BindingProvider> {
+		// Classes
+		private:
+			class Internals;
+
+		// Methods
+		public:
+						// Lifecycle Methods
+						BindingProvider(const IInspectable& bindingProviderAdapter);
+						~BindingProvider();
+
+						// Instance methods
+			bool		Boolean1() const;
+			void		Boolean1(bool value);
+
+			hstring		String1() const;
+			void		String1(hstring value);
+
+			event_token	PropertyChanged(const PropertyChangedEventHandler& handler);
+			void		PropertyChanged(const event_token& token) noexcept;
+
+		// Properties
+		private:
+			Internals*	mInternals;
+	};
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+// MARK: - winrt::WinRTToolbox::factory_implementation
+
+namespace winrt::WinRTToolbox::factory_implementation {
+
+	// MARK: BindingProvider
+	struct BindingProvider : BindingProviderT<BindingProvider, implementation::BindingProvider> {};
+}
