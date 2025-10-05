a64da7c31cc1619b832b35d9334f6cd07b511831
Stevo Brock <stevo@sunsetmagicwerks.com>
2025-10-04 22:45:51 -0700

    Added CString+Extensions and CData+Extensions to add Inspectables.

---
+++ C++ Toolbox Extensions/CData+Extensions.h
@@ -0,0 +1,10 @@
+//----------------------------------------------------------------------------------------------------------------------
+//	CData+Extensions.h			©2025 Stevo Brock	All rights reserved.
+//----------------------------------------------------------------------------------------------------------------------
+
+#pragma once
+
+#include "CData.h"
+#include "TInspectable.h"
+
+DEFINE_TCRInspectable(DataCRInspectable, CData);
---
+++ C++ Toolbox Extensions/CString+Extensions.h
@@ -0,0 +1,10 @@
+//----------------------------------------------------------------------------------------------------------------------
+//	CString+Extensions.h			©2025 Stevo Brock	All rights reserved.
+//----------------------------------------------------------------------------------------------------------------------
+
+#pragma once
+
+#include "CString.h"
+#include "TInspectable.h"
+
+DEFINE_TCVInspectable(OVStringCVInspectable, OV<CString>);
