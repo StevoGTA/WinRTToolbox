ff40469269e057288982c7e9724f52c1ac25a7b4
Stevo Brock <stevo@sunsetmagicwerks.com>
2025-11-28 00:26:49 -0800

    Added STimecode+Extensions.h

---
+++ C++ Toolbox Extensions/STimecode+Extensions.h
@@ -0,0 +1,10 @@
+//----------------------------------------------------------------------------------------------------------------------
+//	STimecode+Extensions.h			©2025 Stevo Brock	All rights reserved.
+//----------------------------------------------------------------------------------------------------------------------
+
+#pragma once
+
+#include "STimecode.h"
+#include "TInspectable.h"
+
+DEFINE_TCVInspectable(TimecodeFrameRateCVInspectable, STimecode::FrameRate);
