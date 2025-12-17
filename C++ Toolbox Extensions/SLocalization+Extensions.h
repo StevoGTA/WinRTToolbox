9726ca74b8a0395a3ed1ef718b66b075ae0a13f5
Stevo Brock <stevo@sunsetmagicwerks.com>
2025-12-17 00:20:03 -0800

    Added SLocalization+Extensions.

---
+++ C++ Toolbox Extensions/SLocalization+Extensions.h
@@ -0,0 +1,10 @@
+//----------------------------------------------------------------------------------------------------------------------
+//	SLocalization+Extensions.h			©2025 Stevo Brock	All rights reserved.
+//----------------------------------------------------------------------------------------------------------------------
+
+#pragma once
+
+#include "SLocalization.h"
+#include "TInspectable.h"
+
+DEFINE_TCVInspectable(LocalizationLanguageCVInspectable, SLocalization::Language);
