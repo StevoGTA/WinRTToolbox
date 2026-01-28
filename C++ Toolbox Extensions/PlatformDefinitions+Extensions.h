8b3a229c567e8c48be9321e1c1f4f95dbf1175e5
Stevo Brock <stevo@sunsetmagicwerks.com>
2026-01-27 17:34:23 -0800

    Added more TInspectables to PlatformDefinitions+Extensions.

--- C++ Toolbox Extensions/PlatformDefinitions+Extensions.h
+++ C++ Toolbox Extensions/PlatformDefinitions+Extensions.h
@@ -6,4 +6,7 @@
 
 #include "TInspectable.h"
 
+DEFINE_TCVInspectable(OVUInt8CVInspectable, OV<UInt8>);
 DEFINE_TCVInspectable(OVUInt16CVInspectable, OV<UInt16>);
+DEFINE_TCVInspectable(OVUInt32CVInspectable, OV<UInt32>);
+DEFINE_TCVInspectable(OVUInt64CVInspectable, OV<UInt64>);
