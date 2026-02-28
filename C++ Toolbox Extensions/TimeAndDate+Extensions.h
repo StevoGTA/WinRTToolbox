936c25a5d95922d76d4e3d2e3e645edbe09f592b
Stevo Brock <stevo@sunsetmagicwerks.com>
2026-02-28 00:29:56 -0800

    Added TimeAndDate+Extensions.h

---
+++ C++ Toolbox Extensions/TimeAndDate+Extensions.h
@@ -0,0 +1,10 @@
+//----------------------------------------------------------------------------------------------------------------------
+//	TimeAndDate+Extensions.h			©2026 Stevo Brock	All rights reserved.
+//----------------------------------------------------------------------------------------------------------------------
+
+#pragma once
+
+#include "TimeAndDate.h"
+#include "TInspectable.h"
+
+DEFINE_TCVInspectable(GregorianDateCVInspectable, SGregorianDate);
